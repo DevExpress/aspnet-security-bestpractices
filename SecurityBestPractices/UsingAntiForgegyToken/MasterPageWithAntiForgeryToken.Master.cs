9f8827616bd95e6384a5d47b668dd1ea3dba6bcc
Dmitry Barakhov <sady4850@gmail.com>
2018-06-08 18:32:53 +0300

    add UsingAntiForgegyToken chapter

---
+++ SecurityBestPractices/UsingAntiForgegyToken/MasterPageWithAntiForgeryToken.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SecurityBestPractices.UsingAntiForgegyToken {
+    public partial class MasterPageWithAntiForgeryToken : System.Web.UI.MasterPage {
+        protected override void OnInit(EventArgs e) {
+            base.OnInit(e);
+            Page.PreLoad += OnPreLoad;
+        }
+        protected void OnPreLoad(object sender, EventArgs e) {
+            if (IsPostBack)
+                System.Web.Helpers.AntiForgery.Validate();
+        }
+        //protected void Page_Init(object sender, EventArgs e) {
+        //    if (IsPostBack) {
+        //        try {
+        //            System.Web.Helpers.AntiForgery.Validate();
+        //        } catch {
+        //            throw new Exception("<b>aaa<b>");
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ SecurityBestPractices/UsingAntiForgegyToken/WebForm1.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="SecurityBestPractices.UsingAntiForgegyToken.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <%= System.Web.Helpers.AntiForgery.GetHtml() %>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Delete" />
+    </form>
+</body>
+</html>
