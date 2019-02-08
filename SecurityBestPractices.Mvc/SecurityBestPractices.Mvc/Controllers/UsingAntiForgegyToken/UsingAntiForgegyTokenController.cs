e5ab0f706a2b8524208ff3df288a1b0f9b9d2c84
VictorSavushkin <victor.savushkin@devexpress.com>
2019-02-08 17:00:05 +0300

    Editable Dashboard antiforgery

--- SecurityBestPractices.Mvc/SecurityBestPractices.Mvc/Controllers/UsingAntiForgegyToken/UsingAntiForgegyTokenController.cs
+++ SecurityBestPractices.Mvc/SecurityBestPractices.Mvc/Controllers/UsingAntiForgegyToken/UsingAntiForgegyTokenController.cs
@@ -1,8 +1,16 @@
-﻿using SecurityBestPractices.Mvc.Models;
+﻿using DevExpress.DashboardWeb;
+using SecurityBestPractices.Mvc.Models;
+using System;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Hosting;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace SecurityBestPractices.Mvc.Controllers {
     public class UsingAntiForgegyTokenController : Controller {
+        /* EditForm */
+
         // GET: /UsingAntiForgegyTokenController/EditForm/
         public ActionResult EditForm() {
             return View("EditForm", EditFormItems.GetList());
@@ -34,5 +42,45 @@ namespace SecurityBestPractices.Mvc.Controllers {
             return EditFormPartial();
         }
 
+
+        /* Dashboard View */
+
+        // GET: /UsingAntiForgegyTokenController/EditDashboard/
+        [Authorize]
+        public ActionResult EditDashboard() {
+            return View("EditDashboard");
+        }
+    }
+
+    /* Dashboard Helper */
+
+    // DashboardValidateAntiForgeryToken Attribute for Dashboard Controlles
+    public sealed class DashboardValidateAntiForgeryTokenAttribute : FilterAttribute, IAuthorizationFilter {
+        public void OnAuthorization(AuthorizationContext filterContext) {
+            if(filterContext == null) {
+                throw new ArgumentNullException(nameof(filterContext));
+            }
+
+            HttpContextBase httpContext = filterContext.HttpContext;
+            HttpCookie cookie = httpContext.Request.Cookies[AntiForgeryConfig.CookieName];
+            AntiForgery.Validate(cookie?.Value, httpContext.Request.Headers["__RequestVerificationToken"]);
+        }
+    }
+
+    /* Dashboard with ValidateAntiForgeryToken */
+    [DashboardValidateAntiForgeryToken]
+    public class DashboardWithAntiForgegyTokenController : DevExpress.DashboardWeb.Mvc.DashboardController {
+        static readonly DashboardConfigurator dashboardConfigurator;
+        static DashboardWithAntiForgegyTokenController() {
+            // sample data
+            var dashboardInMemoryStorage = new DashboardInMemoryStorage();
+            dashboardInMemoryStorage.RegisterDashboard("editId", XDocument.Load(HostingEnvironment.MapPath(@"~/App_Data/PublicDashboard.xml")));
+
+            dashboardConfigurator = new DashboardConfigurator();
+            dashboardConfigurator.SetDashboardStorage(dashboardInMemoryStorage);
+        }
+
+        public DashboardWithAntiForgegyTokenController() : base(dashboardConfigurator) {
+        }
     }
 }
