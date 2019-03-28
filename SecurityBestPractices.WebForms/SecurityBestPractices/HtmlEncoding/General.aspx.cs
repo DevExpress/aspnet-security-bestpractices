2ecb1b532b76cf2f1fbe1744d5a8180e9a1e032e
VictorSavushkin <victor.savushkin@devexpress.com>
2019-03-28 18:06:38 +0300

    site map

---
+++ SecurityBestPractices.WebForms/SecurityBestPractices/HtmlEncoding/General.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SecurityBestPractices.HtmlEncoding {
+    public partial class General : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            if(IsPostBack) {
+                // *** Input ***
+                // not secure
+                //SearchResultLiteral.Text = string.Format("Your search - {0} - did not match any documents.", SearchBox.Text);
+
+                // secure
+                SearchResultLiteral.Text = string.Format("Your search - {0} - did not match any documents.", HttpUtility.HtmlEncode(SearchBox.Text));
+                // or ASPxLabel
+                SearchResultLabel.Text = string.Format("Your search - {0} - did not match any documents.", SearchBox.Text);
+            }
+
+            // *** URL ***
+            // not secure
+            //if(Request.QueryString["returnUrl"] != null)
+            //    urlLink.HRef = Request.QueryString["returnUrl"].ToString();
+
+            // secure
+            if(Request.QueryString["returnUrl"] != null)
+                urlLink.HRef = HttpUtility.HtmlEncode(Request.QueryString["returnUrl"].ToString());
+        }
+    }
+}
